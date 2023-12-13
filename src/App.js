266a17280f23ee5e1dff4a13eb00af70b186a97b
MD Nabeel Ahemad <mdnabeelahemad539@gmail.com>
2023-12-13 16:54:31 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ function App() {
     .catch(error => console.log(error))
   }, [term])
   return (
-    <div className='container mx-auto mb-10'>
+    <div className='container mx-auto mb-10 '>
       <ImageSearch searchText={(text) => setTerm(text)}/>
 
       {!isLoading && images.length === 0 && <h1 className='text-5xl text-center mx-auto mt-32'>No Images Found</h1>}
 
-      {isLoading ? <h1 className='text-6xl text-center mx-auto mt-32'>Loading....</h1>
+      {isLoading ? <h1 className='text-6xl text-center mx-auto mt-32 sm:text-xl'>Loading....</h1>
       :<div className='grid grid-cols-1 gap-2 2xl:grid-cols-4 2xl:gap-2 xl:grid-cols-4 xl:gap-2 md:grid-cols-2 lg:grid-cols-3'>
         {images.map(image =>(
           <ImageCard key={image.id} image={image}/>
